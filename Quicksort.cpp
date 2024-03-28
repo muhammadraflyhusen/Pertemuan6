9e98a293a5403bae808f6a711874a6ee28b493eb
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-03-28 15:34:35 +0700

    variabel

---
+++ Quicksort.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+//array of integers to hold value 
+int arr[20];
+int cmp_count = 0; //number of comparasion
+int mov_count = 0; //number of movements
+int n;
+
